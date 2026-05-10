3d5e1cca8fa2c06e5eb818f296c39df90677ddbe
Erin Shea <e.l.shea23@gmail.com>
2026-05-10 14:29:18 -0500

    player attack and player movement

---
+++ PlayerAttackSimple.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class PlayerAttackSimple : MonoBehaviour
+{
+    public float attackRange = 1.5f;
+    public int damage = 1;
+    public LayerMask enemyLayer;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, attackRange, enemyLayer);
+
+        foreach (Collider2D hit in hits)
+        {
+            EnemyHealthSimple enemy = hit.GetComponent<EnemyHealthSimple>();
+
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+    }
+}
